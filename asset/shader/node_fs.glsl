a63f291533fadd04f242969bd458d2686a36aaf2
robotberryer <robotberrykun@gmail.com>
2021-03-21 15:03:05 +0800

    删除多余的shader

--- asset/shader/node_fs.glsl
+++
@@ -1,8 +0,0 @@
-#version 420
-
-out vec4 FColor;
-
-void main()
-{
-	FColor = vec4(1.0, 0.0, 0.0, 1.0);
-}
--- asset/shader/section_fs.glsl
+++
@@ -1,9 +0,0 @@
-#version 420
-
-in vec3 VTexcoord;
-out vec4 FColor;
-
-void main()
-{
-	FColor = vec4(1.0, 1.0, 1.0, 1.0);
-}
